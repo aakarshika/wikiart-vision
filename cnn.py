66d784560979201411a74778fe41ab371276067a
Bhavika <bhavicka.7992@gmail.com>
2018-05-10 01:09:40 -0400

    2 layer CNN

--- cnn.py
+++ cnn.py
@@ -1,18 +1,37 @@
-import torch.utils.data as data_utils
 import torch
 import pandas as pd
 from torch.autograd import Variable
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
-from torchvision.models import resnet18
 import itertools
-from dotenv import load_dotenv, find_dotenv
 import pickle
 from wikiart import WikiartDataset
+import torch.utils.data as data_utils
+from dotenv import load_dotenv, find_dotenv
 import os
-from time import time
 
 load_dotenv(find_dotenv())
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16*5*5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16*5*5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
 
 def get_classes(filepath):
@@ -20,18 +39,6 @@ def get_classes(filepath):
     return list(data['Genre'].unique()), list(data['Class'].unique())
 
 genres, classes = get_classes(os.getenv('mappings'))
-
-class _classifier(nn.Module):
-    def __init__(self, pretrained_model, n_classes):
-        super(_classifier, self).__init__()
-        self.features = nn.Sequential(*list(pretrained_model.children())[:-1])
-        self.classifier = nn.Sequential(nn.Linear(512, n_classes))
-
-    def forward(self, x):
-        f = self.features(x)
-        f = f.view(f.size(0), -1)
-        y = self.classifier(f)
-        return y
 
 
 def save_checkpoint(state, learning_rate, epochs, path='models/', filename='resnet18_{}_{}_checkpoint.pth.tar'):
@@ -66,23 +73,20 @@ def main(learning_rate, epochs=20):
     print("Test images path", test_images_path)
 
     print("Loading train data....")
-    wiki_train = WikiartDataset(config={'wikiart_path': train_path, 'images_path': train_images_path, 'size': train_size,
-                                        'arch': 'resnet', 'train': True})
+    wiki_train = WikiartDataset(
+        config={'wikiart_path': train_path, 'images_path': train_images_path, 'size': train_size,
+                'arch': 'cnn', 'train': True})
 
     print("Loading test data....")
     wiki_test = WikiartDataset(config={'wikiart_path': test_path, 'images_path': test_images_path, 'size': test_size,
-                                       'arch': 'resnet', 'train': False})
+                                       'arch': 'cnn', 'train': False})
 
     wiki_train_dataloader = data_utils.DataLoader(wiki_train, batch_size=bs, shuffle=True, num_workers=4,
                                                   drop_last=False)
     wiki_test_dataloader = data_utils.DataLoader(wiki_test, batch_size=bs, shuffle=True, num_workers=4,
                                                  drop_last=False)
 
-    # net = _classifier(pretrained_model=resnet18(pretrained=True), n_classes=15)
-
-    pretrained_model = resnet18(pretrained=True)
-
-    net = _classifier(pretrained_model=pretrained_model, n_classes=10)
+    net = Net()
 
     # Defining the loss function
     criterion = nn.CrossEntropyLoss()
@@ -98,7 +102,7 @@ def main(learning_rate, epochs=20):
             inputs, labels = data['image'], data['class']
             batchsize = inputs.shape[0]
             # make this 4, 32, 32, 3 -> 4, 3, 32, 32
-            inputs = inputs.view(batchsize, 3, 224, 224)
+            inputs = inputs.view(batchsize, 3, 32, 32)
 
             inputs, labels = Variable(inputs), Variable(labels)
             optimizer.zero_grad()
@@ -112,28 +116,24 @@ def main(learning_rate, epochs=20):
 
             # print statistics
             running_loss += loss.data[0]
-            if i % 50 == 49:  # print every 50 mini-batches
+            if i % 10 == 49:  # print every 10 mini-batches
                 print('[%d, %5d] loss: %.3f' %
                       (epoch + 1, i + 1, running_loss / 2000))
                 running_loss = 0.0
 
     print('Finished Training')
-    loc = save_checkpoint({'epoch': epochs, 'arch': 'resnet18_re', 'state_dict': net.state_dict(), 'model': net},
-                          learning_rate=learning_rate, epochs=epochs)
-
-    net, state = load_model(loc)
-
+    save_checkpoint({'epoch': epochs, 'arch': 'CNN_2Layer', 'state_dict': net.state_dict(), 'model': net},
+                    learning_rate=learning_rate, epochs=epochs)
     print("Predicting on the test set... ")
     class_correct = [i for i in range(n_classes)]
     class_total = [100] * n_classes
 
     y_pred = []
     y_actual = []
-
     for data in wiki_test_dataloader:
         images, labels = data['image'], data['class']
         batchsize = images.shape[0]
-        images = images.view(batchsize, 3, 224, 224)
+        images = images.view(batchsize, 3, 32, 32)
         outputs = net(Variable(images))
         _, predicted = torch.max(outputs.data, 1)
         c = (predicted == labels).squeeze()
@@ -147,28 +147,28 @@ def main(learning_rate, epochs=20):
     print("Correct classes", class_correct)
     print("Total count for each class", class_total)
 
-    print("Pickling predictions and labels")
-    pkl1_name = "rnet18_re_ypred_{}_{}.pkl".format(learning_rate, epochs)
-    pkl2_name = "rnet18_re_y_actual_{}_{}.pkl".format(learning_rate, epochs)
+    pkl1_name = "cnn_ypred_{}_{}.pkl".format(learning_rate, epochs)
+    pkl2_name = "cnn_y_actual_{}_{}.pkl".format(learning_rate, epochs)
 
-    with open(pickle_path + pkl1_name, 'wb') as f:
+    with open(pickle_path+pkl1_name, 'wb') as f:
         pickle.dump(y_pred, f)
 
-    with open(pickle_path + pkl2_name, 'wb') as f2:
+    with open(pickle_path+pkl2_name, 'wb') as f2:
         pickle.dump(y_actual, f2)
 
     for i in range(len(classes)):
         print('Accuracy of %5s : %2d %%' % (
-            genres[i], float(class_correct[i].item() * 100)/ class_total[i]))
+            genres[i], 100 * class_correct[i] / class_total[i]))
 
 
 if __name__ == '__main__':
 
     lrs = [1e-5, 1e-4, 1e-3, 1e-2, 1e-1]
-    epochs = [20]
+    epochs = [100]
 
     combinations = list(itertools.product(lrs, epochs))
 
-    for c in combinations:
-        print("Learning rate {}, no of epochs {}".format(c[0], c[1]))
-        main(learning_rate=c[0], epochs=c[1])
+    # for c in combinations:
+    #     print("Learning rate {}, no of epochs {}".format(c[0], c[1]))
+    #     main(learning_rate=c[0], epochs=c[1])
+    main(learning_rate=0.001, epochs=20)
--- resnet_eval.py
+++ resnet_eval.py
@@ -1,7 +1,39 @@
-import pandas as pd
-import os
-from dotenv import load_dotenv, find_dotenv
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
-from time import time
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+import pickle
+import itertools
+from pprint import pprint
 
+lrs = [1e-5, 1e-4, 1e-3, 1e-2]
+epochs = [20]
+
+combinations = list(itertools.product(lrs, epochs))
+
+for c in combinations:
+    actual = 'models/rnet18_re_y_actual_{}_{}.pkl'.format(c[0], c[1])
+    pred = 'models/rnet18_re_ypred_{}_{}.pkl'.format(c[0], c[1])
+
+    y = pickle.load(open(actual, 'rb'))
+    y_pred = pickle.load(open(pred, 'rb'))
+
+    y_act_list = []
+    y_pred_list = []
+
+    for i in y:
+        y_act_list.append(i.item())
+    for i in y_pred:
+        y_pred_list.append(i.item())
+
+    print("For learning rate {}".format(c[0]))
+    print("Accuracy", accuracy_score(y_act_list, y_pred_list) * 100)
+
+    pprint(classification_report(y_act_list, y_pred_list, target_names=['abstract_painting',
+                                                                       'cityscape',
+                                                                       'genre_painting',
+                                                                       'illustration', 'landscape',
+                                                                       'nude_painting',
+                                                                       'portrait',
+                                                                       'religious_painting',
+                                                                       'sketch_and_study',
+                                                                       'still_life']))
